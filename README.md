be32cba3b9fa1337db19109411d5cb213992a0f4
Chuck King <cwkingjr@gmail.com>
2025-08-11 18:03:55 -0500

    add a bunch of error checking

--- README.md
+++ README.md
@@ -72,6 +72,39 @@ uv tool upgrade tims-cli-tools
 uv tool uninstall tims-cli-tools
 ```
 
+## TIMS BU Info
+
+### Description
+
+This tool accepts a BU number as it's command line argument, looks in the master tracker spreadsheet to find that BU, grabs some of the info from that row, and prints it into the terminal screen. The idea behind this tool is to allow you to very quickly grab address and lat/long for a BU instead of having to pull up the spreadsheet and find it yourself.
+
+### Set Up a Configuration File
+
+This tool requires a configuration file in a hidden directory so it can read that file to discover where the master tracker spreadsheet is located.
+
+Either grab the `tims_tools.toml` file from this repo or create one exactly like it from scratch with Notepad.
+
+The file contents need to be exactly the same as the example in this repo, but with the path in quotes changed to reflect where the production master tracker spreadsheet lives.
+
+This file must be in your home folder, under some additional folders, like this: `/Users/<your-user-name>/.config/tims_tools/tims_tools.toml`. That `.config` directory starts with a dot on purpose as thats a hidden folder (typically).
+
+We're using this system of folders because that is convention for command line tool configs. That way, other tools can put their configs in `.config/` under a folder named for their tool, and can put whatever files they need under their folder.
+
+If you are interested in using Git Bash, you can go into that terminal and run this command: `mkdir -p ~/.config/tims_tools/` to make those folders all at one time. Then place your modified tims_tools.toml file into that tims_tools directory/folder.
+
+### Invoke TIMS BU Info
+
+To run the tool, got to the Git Bash or PS Terminal and run (`<something>` inside angle brackets indicates this is something your are supposed to replace, replacing angles and all; meaning, leave out the angles.):
+
+```bash
+tims_bu_info <bu number>
+tims_bu_info 4746458576
+```
+
+Example output:
+
+<p align="center"><img src="images/bu_info_output.png" width="400" /></p>
+
 ## TIMS Invoice Transformer
 
 The tims_invoice_transformer tool reads a spreadsheet extracted from the master tracing spreadsheet, grabs the columns it needs, and generates an output spreadsheet with the data transformed into the format required for pasting into a specific third party's invoice submission spreadsheet.
--- src/tims_cli_tools/tims_bu_info.py
+++ src/tims_cli_tools/tims_bu_info.py
@@ -1,21 +1,29 @@
 import sys
-from datetime import datetime
+import tomllib
 from pathlib import Path
 
 import pandas as pd
-import pytz
 from rich.pretty import pprint
-import tomllib
+
+PATH_SUFFIX = ".config/tims_tools/tims_tools.toml"
 
 
 def get_toml_data() -> dict:
     home = Path.home()
-    config_path = home / ".config/tims_tools/tims_tools.toml"
+    config_path = home / PATH_SUFFIX
+
+    if not Path.exists(config_path):
+        pprint(f"Whoops, no config file exists at {config_path}")
+        pprint(
+            "Please see the repo directions and make sure you have the config file in this location."
+        )
+        sys.exit(1)
+
     try:
         with Path.open(config_path, "rb") as f:
             toml_data = tomllib.load(f)
-    except OSError() as e:
-        msg = f"Problem with loading config file. Expected config file at {config_path}"
+    except IOError as e:
+        msg = f"Problem with loading toml data from the config file. Please check the settings in your config file to ensure they match the config file in the repo."
         raise OSError(msg) from e
 
     return toml_data
@@ -23,20 +31,40 @@ def get_toml_data() -> dict:
 
 def main():
     if len(sys.argv) < 2:
-        pprint(f"Usage: {sys.argv[0]} BUN")
+        pprint("Whoops, no BU passed in")
+        pprint("Usage: tims_bu_info <BUN>")
+        pprint("Example: tims_bu_info 22222223")
         sys.exit(1)
 
-    timezone_name = "US/Central"
-    # Create a tzinfo object from the timezone name
-    tz = pytz.timezone(timezone_name)
-
+    # grab the bu number passed in and clean it up
     bun = sys.argv[1]
     bun = int(bun.strip())
 
     toml_data = get_toml_data()
-    master_tracker_path = Path(toml_data["tims_bu_info"]["master_tracker_path"])
-    input_df = pd.read_excel(master_tracker_path)
 
+    if "tims_bu_info" not in toml_data:
+        pprint("Whoops, couldn't find '[tims_bu_info]' section in your config file.")
+        sys.exit(1)
+
+    if "master_tracker_path" not in toml_data["tims_bu_info"]:
+        pprint("Whoops, couldn't find 'master_tracker_path' in your config file.")
+        sys.exit(1)
+
+    try:
+        # pull the tracker path out of the toml settings
+        master_tracker_path = Path(toml_data["tims_bu_info"]["master_tracker_path"])
+    except ValueError as e:
+        msg = "Sorry, looks like something is wrong with the content of your config file. Please compare to the tims_tools.toml in the GitHub repo."
+        raise ValueError(msg) from e
+
+    try:
+        # read the spreadsheet into a dataframe
+        input_df = pd.read_excel(master_tracker_path)
+    except OSError as e:
+        msg = f"Problem reading master tracker xlsx from {master_tracker_path}"
+        raise OSError(msg) from e
+
+    # should probably live in field.py
     fields = [
         "BU",
         "Crew",
@@ -50,9 +78,19 @@ def main():
         "Lat",
         "Long",
     ]
+
+    # the tracker has a lot of fields we don't need, so load the bun_df with only the fields we want
     bun_df = input_df[fields]
+
+    # Find the row that matches the bun passed in
     bun_df = bun_df.loc[bun_df["BU"] == bun]
-    print(bun_df.transpose())
+
+    # If we didn't find that bun, let the user know
+    if bun_df.empty:
+        print(f"Sorry, no BU with that number ({bun}) found.")
+    else:
+        # print the row vertically to make it easier to see, copy, etc
+        print(bun_df.transpose())
 
 
 if __name__ == "__main__":
