e42bdc2891a815bec7474c333255b40389ded132
Chuck King <cwkingjr@gmail.com>
2025-08-17 20:36:22 -0500

    wip got some tests running

--- src/tims_cli_tools/tims_invoice_transformer.py
+++ src/tims_cli_tools/tims_invoice_transformer.py
@@ -12,9 +12,13 @@ from rich.pretty import pprint
 from . import desc, field, price, subcat
 
 
-def check_for_required_fields(df: pd.DataFrame) -> None:
+def check_for_required_fields(
+    *, required_fields: list[str], pd_df: pd.DataFrame
+) -> None:
     """Check if all required fields are present in the DataFrame."""
-    missing_fields = set(field.REQUIRED_FIELDS) - set(df.columns)
+    required_field_set = set(required_fields)
+    df_columns_set = set(pd_df.columns.to_list())
+    missing_fields = required_field_set - df_columns_set
     if missing_fields:
         raise ValueError("Missing required fields: " + ", ".join(missing_fields))
 
@@ -52,16 +56,50 @@ def ensure_int(*, number=None, col_name: str, row: pd.Series) -> bool:
         return True
 
 
+def get_value_from_series_col(*, series: pd.Series, field_name: str):
+    mylist = [
+        col_value for (col_name, col_value) in series.items() if col_name == field_name
+    ]
+    if not mylist:
+        msg = f"Could not find value for field {field_name} in {series}"
+        raise ValueError(msg)
+    return mylist[0]
+
+
+def build_new_rows_from_dataframe_col_values(*, dataframe: pd.DataFrame) -> list[dict]:
+    """Creates a list of new row dicts.
+
+    Dict keys included only the 4-5 cols needed for the derived rows. Dict values are
+    based on source data column-specific processing rules and the values in those source columns.
+    """
+    # iterate the rows and build new subordinate rows based upon the data in the pertinent columns
+    all_new_rows = []
+    for _, row in dataframe.iterrows():
+        one_rows_new_rows = create_derived_rows_list(row)
+        if one_rows_new_rows:
+            all_new_rows.extend(one_rows_new_rows)
+    return all_new_rows
+
+
+def create_cleaned_filepath(
+    *, in_path=Path, filename_prefix: str, dt: datetime
+) -> Path:
+    """Create a new filepath with cleaned up and appended filename."""
+    path = Path(in_path)
+    filename_no_extension = path.stem
+    filename_extension = path.suffix
+    path_parent = path.parent
+    add_to_filename = f"_{filename_prefix}_{dt.strftime('%Y%m%d_%H%M%S')}"
+    cleaned_filename = filename_no_extension.strip().replace(" ", "_")
+    new_filename = cleaned_filename + add_to_filename + filename_extension
+    cleaned_full_path = path_parent / new_filename
+    return cleaned_full_path
+
+
 def create_derived_rows_list(row: pd.Series) -> list[dict]:
     """Create derived rows based on the input row."""
-    current_bu = None
-    current_sort_by = None
-    while current_bu is None or current_sort_by is None:
-        for colname, colvalue in row.items():
-            if colname == field.SORT_BY:
-                current_sort_by = colvalue
-            elif colname == field.BU:
-                current_bu = colvalue
+    current_bu = get_value_from_series_col(series=row, field_name=field.BU)
+    current_sort_by = get_value_from_series_col(series=row, field_name=field.SORT_BY)
 
     new_rows = []
 
@@ -69,7 +107,7 @@ def create_derived_rows_list(row: pd.Series) -> list[dict]:
     row = row.dropna()
 
     for col_name, col_value in row.items():
-        if col_name == field.HVF and ensure_float(
+        if col_name == field.HVF_NO_SPACE and ensure_float(
             number=col_value,
             col_name=col_name,
             row=row,
@@ -221,17 +259,17 @@ def main() -> None:
     in_file = sys.argv[1]
     input_df = pd.read_excel(in_file)
 
-    check_for_required_fields(input_df)
+    check_for_required_fields(required_fields=field.REQUIRED_INPUT_COLS, pd_df=input_df)
 
     # typically the provided spreadsheet has more columns than we need, so we select only the ones we need
-    wanted_df = input_df[field.REQUIRED_FIELDS]
+    wanted_df = input_df[field.REQUIRED_INPUT_COLS]
 
     # rename columns to match the submission template
     wanted_df = wanted_df.rename(
         columns={
-            "Base for Inv.": field.DESCRIPTION,
-            "Additional Canister Level": field.ADD_CAN,
-            "HVF ": field.HVF,  # remove trailing space
+            field.BASE_FOR_INV: field.DESCRIPTION,
+            field.ADD_CAN_LEVEL: field.ADD_CAN_PRICE,
+            field.HVF_WITH_SPACE: field.HVF_NO_SPACE,
         },
     )
 
@@ -242,46 +280,41 @@ def main() -> None:
     wanted_df[field.QUANTITY] = 1
 
     # sort the rows by the "BU" column
-    # this is critical to ensure that the derived rows are grouped with their parent row and everything is in a predictable order
+    # this is critical to ensure that eventually the derived rows are grouped with their parent row and everything is in a predictable order
     wanted_df = wanted_df.sort_values(by=[field.BU])
+
     # add a "SORT_BY" column using a series of integers starting at 1000000 and incrementing by 100
     # this allows us to insert derived rows in between the base rows later
     wanted_df[field.SORT_BY] = range(1000000, 1000000 + 100 * len(wanted_df), 100)
 
-    # add an "EXTRA_CANS" column that includes the value from the "Additional Canister Level" column only if that column includes an integer value
+    # add an "EXTRA_CANS" column that includes the value from the structure column only if that column includes an integer value
     # the first can is part of the base price, so we subtract 1 from the value
     wanted_df[field.EXTRA_CANS] = wanted_df[field.STRUCTURE].apply(
         lambda x: x - 1 if isinstance(x, int) and x - 1 > 0 else None,
     )
-    # Convert this to int that can handle NaN values as we were getting floats before
+
+    # Convert column to dataframe int type that can handle NaN values as we were getting floats before
     wanted_df[field.EXTRA_CANS] = wanted_df[field.EXTRA_CANS].astype(pd.Int64Dtype())
 
     # reorder the columns to match the submission template
-    wanted_df = wanted_df[field.OUTPUT_COLUMNS]
+    wanted_df = wanted_df[field.OUTPUT_COLS]
 
     # now that we have the original data straightened out, let's get on with creating the derived rows
 
-    # iterate the rows and build new subordinate rows based upon the data in the pertinent columns
-    all_new_rows = []
-    for _, row in wanted_df.iterrows():
-        new_rows = create_derived_rows_list(row)
-        if new_rows:
-            all_new_rows.extend(new_rows)
+    derived_rows = build_new_rows_from_dataframe_col_values(dataframe=wanted_df)
+    # build a dataframe so we can concat the new rows into the original rows
+    derived_rows_df = pd.DataFrame(derived_rows)
+    wanted_df = pd.concat([wanted_df, derived_rows_df], ignore_index=True)
 
-    new_rows_df = pd.DataFrame(all_new_rows)
-    wanted_df = pd.concat([wanted_df, new_rows_df], ignore_index=True)
-
+    # sort the final dataframe so the rows are in the customer-specified order in the spreadsheet
     wanted_df = wanted_df.sort_values(by=[field.SORT_BY], ascending=True)
 
-    # Create a new filepath with cleaned up and appended filename
-    path = Path(in_file)
-    filename_no_extension = path.stem
-    filename_extension = path.suffix
-    path_parent = path.parent
-    add_to_filename = f"_transformed_{datetime.now(tz=tz).strftime('%Y%m%d_%H%M%S')}"
-    cleaned_filename = filename_no_extension.strip().replace(" ", "_")
-    new_filename = cleaned_filename + add_to_filename + filename_extension
-    cleaned_full_path = path_parent / new_filename
+    # create an output file path based upon the old file path but with a name that indicates the
+    # output has been transformed and give it a new datetime each time so user can always see
+    # when it was generated
+    cleaned_path = create_cleaned_filepath(
+        in_path=in_file, filename_prefix="transformed", dt=datetime.now(tz=tz)
+    )
 
-    wanted_df.to_excel(cleaned_full_path, index=False)
-    pprint(f"Wrote new transformed spreadsheet at: {cleaned_full_path}")
+    wanted_df.to_excel(cleaned_path, index=False)
+    pprint(f"Wrote new transformed spreadsheet at: {cleaned_path}")
